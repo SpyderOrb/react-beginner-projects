58bfc5681795489a78a9de7993abd52d30a7ce71
SpyderOrb <alex.plachinda@gmail.com>
2023-03-15 23:00:53 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './index.scss';
+import React, { useState } from 'react'
+import './index.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+	const [count, setCount] = useState(0)
+	const onClickPlus = () => {
+		setCount(count + 1)
+	}
+	const onClickMinus = () => {
+		setCount(count - 1)
+	}
+	return (
+		<div className='App'>
+			<div>
+				<h2>Счетчик:</h2>
+				<h1>{count}</h1>
+				<button onClick={onClickMinus} className='minus'>
+					- Minus
+				</button>
+				<button onClick={onClickPlus} className='plus'>
+					Plus +
+				</button>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
